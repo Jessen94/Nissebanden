618538d81c3006ce744d1d53e20d06294b07ad9d
emilj <emil.jessen94@gmail.com>
2020-03-25 22:05:32 +0100

    Put fb embed in seperate component

--- dist/src/App.js
+++ dist/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './App.css'
 import { Box } from '@material-ui/core'
+import FacebookFrame from './components/FacebookFrame';
 
 function App() {
   return (
     <Box>
-      <header class="header-container">
-        <div class="header-content">
+      <header className="header-container">
+        <div className="header-content">
           <h1>
             <span>
               Velkommen til Klan Nissebanden - En del af solvanggruppe
@@ -15,8 +16,8 @@ function App() {
         </div>
       </header>
       <div>
-        <div class="content-column">
-          <div class="content-column-text">
+        <div className="content-column">
+          <div className="content-column-text">
             <h2>
               <span>
                 For klanspejderne på 16-24 år er fællesskabet
@@ -34,24 +35,17 @@ function App() {
             </span>
             </h2>
           </div>
-          <div class="content-column-images-right">
-            <img data-lazy="Images/Slovakiet_2019_bjerg.jpg" />
+          <div className="content-column-images-right">
+            <img src="Images/Slovakiet_2019_bjerg.jpg" alt="Klanen på et bjerg i slovakiet" />
           </div>
           </div>
+ 
 
-
-
-          <div class="white-space">
-            <div class="white-space-block">
-
-            </div>
+          <div className="content-column">
+            <div className="content-column-images-left">
+              <img src="Images/Slovakiet_2019_Paintball.jpg" alt="Klanen spiller paintball"/>
           </div>
-
-          <div class="content-column">
-            <div class="content-column-images-left">
-              <img data-lazy="Images/Slovakiet_2019_Paintball.jpg" />
-          </div>
-              <div class="content-column-text">
+              <div className="content-column-text">
                 <h2>
                   <span>
                     For mere info tjek vores facebook-fanside -
@@ -63,23 +57,15 @@ function App() {
               </div>
             </div>
 
-            <div class="white-space">
-              <div class="white-space-block">
-              </div>
-            </div>
-
-            <div class="fb-embed">
-              <iframe
-                src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FNissebanden%2F&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
-                scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
-            </div>
-            <footer class="footer">
-              <p class="footer-links">
+            <FacebookFrame />
+           
+            <footer className="footer">
+              <p className="footer-links">
                 <a href="https://www.Nissebanden.dk">Home</a>
                 <a href="https://www.facebook.com/pg/Nissebanden/about/">About</a>
                 <a href="https://www.facebook.com/Nissebanden/">Contact</a>
               </p>
-              <p class="footer-company-name">Klan Nissebanden &copy; 1991</p>
+              <p className="footer-company-name">Klan Nissebanden &copy; 1991</p>
             </footer>
           </div>
     </Box>
