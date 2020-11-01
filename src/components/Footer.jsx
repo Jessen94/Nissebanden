44b71e82fba76e5a348fc1cc1c1665dbccbd6dfd
emilj <emil.jessen94@gmail.com>
2020-11-01 15:44:54 +0100

    Updates

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Box } from '@material-ui/core';
+
+
+
+const Footer = () => {
+  return (
+    <Box className="footer">
+      <p className="footer-links">
+        <a className="footer-links" href="https://www.Nissebanden.dk">Home</a>
+        <a className="footer-links" href="https://www.facebook.com/pg/Nissebanden/about/">About</a>
+        <a className="footer-links" href="https://www.facebook.com/Nissebanden/">Contact</a>
+      </p>
+      <p className="footer-company-name">Klan Nissebanden &copy; 1991</p>
+    </Box>
+  );
+};
+
+export default Footer;
