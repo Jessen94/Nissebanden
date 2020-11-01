c9a8b15c942f7b9a4b60ccbf1459cd94c19a9eac
emilj <emil.jessen94@gmail.com>
2020-11-01 15:34:15 +0100

    Moved to react

--- dist/src/App.jsx
+++ dist/src/App.jsx
@@ -1,108 +1,91 @@
 import React from 'react';
 import './App.css';
-import { Box } from '@material-ui/core';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import FacebookFrame from './components/FacebookFrame';
-import Footer from './components/Footer';
 import Img from './components/Img';
-import BackgroundImage from './components/Images/Banner.JPG';
 
-const useStyles = makeStyles({
-  contentColumn: {
-    display: 'flex',
-    alignItems: 'center',
-    marginLeft: '20%',
-    marginRight: '20%',
-    marginBottom: '10%',
-    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
-  },
-  headerContainer: {
-    display: 'flex',
-    alignItems: 'flex-end',
-    justifyContent: 'right',
-    height: '800px',
-    backgroundSize: 'cover',
-    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
-    padding: '0px',
-  },
-  headerContent: {
-    width: '100%',
-    fontSize: '48px',
-    padding: '0%',
-  },
-  CardContent: {
-    padding: '0px',
-  },
-  contentColumnText: {
-    width: '100%',
-    overflow: 'auto',
-    padding: '8%',
-    lineHeight: '1.4',
-    textAlign: 'left',
-  },
-  text: {
-    width: '30%',
-    margin: '5%',
-    color: '#edebe9',
-  },
-});
+const App = () => {
 
-function App() {
-  const classes = useStyles();
   return (
-    <Box>
-      <header>
-        <div className={classes.headerContent}>
-          <CardContent className={classes.CardContent}>
-            <CardMedia
-              className={classes.headerContainer}
-              image={BackgroundImage}
-            >
-              <Typography variant="h3" className={classes.text}>
-                Velkommen til Klan Nissebanden - En del af solvanggruppe
-              </Typography>
-            </CardMedia>
-          </CardContent>
-        </div>
-      </header>
-      <div>
-        <Box className={classes.contentColumn}>
-          <div className={classes.contentColumnText}>
-            <Typography variant="body1" gutterBottom>
-              For klanspejderne på 16 - 26 år er fællesskabet
-              og samværet med ens spejderkammerater i centrum.
-              Her har spejderne selv ansvar for arbejdet i patruljen,
-              de vælger selv deres leder, og de bestemmer
-              selv deres aktiviteter. De tager ofte på ture til
-              internationale lejre. Det er det udfordrende og
-              uafhængige spejderliv, som bliver afprøvet.
-              Spejderne selv har ansvaret for at udvikle og
-              gennemføre projekter. Det giver dem viden og
-              udvikler deres evner. For de ældste spejdere
-              kan et projekt godt vare længe, være stort
-              eller ske i samarbejde med spejdere fra andre grupper.
-            </Typography>
-          </div>
-          <Img alt="Klanen på et bjerg i slovakiet" left />
-        </Box>
-        <Box className={classes.contentColumn} style={{ direction: 'rtl' }}>
-          <div className={classes.contentColumnText}>
-            <Typography variant="body1" gutterBottom>
-              For mere info tjek vores facebook-fanside -
-              hvor der blandt andet kan findes en beskrivelse
-              af historien bag navnet, en track-artikel,
-              og billeder fra sidste års sommerlejr, som var en vandretur i Slovakiet.
-            </Typography>
-          </div>
-          <Img alt="Klanen er til paintball" left={false} />
-        </Box>
-        <FacebookFrame />
-        <Footer />
+    <div> 
+  <header className="header-container">
+    <div className="header-content">
+      <h1>
+        <span>
+          Velkommen til Klan Nissebanden - En del af solvanggruppe
+        </span>
+      </h1>
+    </div>
+  </header>
+ 
+    <div className="content-column">
+      <div className="content-column-text">
+        <h2>
+          <span>
+          For klanspejderne på 16-24 år er fællesskabet 
+          og samværet med ens spejderkammerater i centrum. 
+          Her har spejderne selv ansvar for arbejdet i patruljen, 
+          de vælger selv deres leder, og de bestemmer 
+          selv deres aktiviteter. De tager ofte på ture til 
+          internationale lejre. Det er det udfordrende og 
+          uafhængige spejderliv, som bliver afprøvet.
+          Spejderne selv har ansvaret for at udvikle og 
+          gennemføre projekter. Det giver dem viden og 
+          udvikler deres evner. For de ældste spejdere 
+          kan et projekt godt vare længe, være stort 
+          eller ske i samarbejde med spejdere fra andre grupper.
+        </span>
+        </h2>
       </div>
-    </Box>
+      <div className="content-column-images-right">
+        <Img alt="" left={false}  />
+      </div>
+    </div>
+ 
+    <div className="white-space">
+      <div className="white-space-block">
+
+      </div>
+    </div>
+ 
+    <div className="content-column">
+      <div className="content-column-images-left">
+      <Img alt="" left={true}  />
+      </div>
+      <div className="content-column-text">
+        <h2>
+          <span>
+          For mere info tjek vores facebook-fanside - 
+          hvor der blandt andet kan findes en beskrivelse 
+          af historien bag navnet, en track-artikel, 
+          og billeder fra sidste års sommerlejr, som var en vandretur i Slovakiet.
+        </span>
+        </h2>
+      </div>
+    </div>
+ 
+    <div className="white-space">
+      <div className="white-space-block">
+
+      </div>
+    </div>
+ 
+    <div className="fb-embed">
+      <iframe
+        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FNissebanden%2F&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+        scrolling="no" frameBorder="0" allow="encrypted-media"></iframe>
+    </div>
+    <footer className="footer">
+  
+        <p className="footer-links">
+          <a href="https://www.Nissebanden.dk">Home</a>
+          ·
+          <a href="https://www.facebook.com/pg/Nissebanden/about/">About</a>
+          ·
+          <a href="https://www.facebook.com/Nissebanden/">Contact</a>
+        </p>
+  
+        <p className="footer-company-name">Klan Nissebanden &copy; 1991</p>
+      </footer>
+    </div>
   );
 }
 
--- dist/src/components/Img.jsx
+++ dist/src/components/Img.jsx
@@ -1,46 +1,33 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
-import { Box } from '@material-ui/core';
 import ImgLeft from './Images/Slovakiet_2019_bjerg.jpg';
 import ImgRight from './Images/Slovakiet_2019_Paintball.jpg';
 
 
-const useStyles = makeStyles({
-  root: {
-    display: 'flex',
-  },
-  cover: {
-    transform: 'scale(0.9)',
-    maxWidth: '150%',
-    height: 'auto',
-    margin: '4%',
-  },
-});
+const Images = ({ image, alt, clas }) => {
 
-const Images = ({ image, alt }) => {
-  const classes = useStyles();
   return (
-    <Box className={classes.root}>
-      <img src={image} alt={alt} className={classes.cover} />
-    </Box>
+    <div className={clas}>
+      <img src={image} alt={alt} />
+    </div>
   );
 };
 
 Images.propTypes = {
-  image: PropTypes.string.isRequired,
+  // eslint-disable-next-line react/no-typos
+  image: PropTypes.string,
   alt: PropTypes.string.isRequired,
+  class: PropTypes.string.isRequired,
 };
 
-// eslint-disable-next-line react/prop-types
 const Img = ({ alt, left }) => {
   if (left) {
     return (
-      <Images image={ImgLeft} alt={alt} />
+      <Images image={ImgLeft} alt={alt} class="content-column-images-right" />
     );
   }
   return (
-    <Images image={ImgRight} alt={alt} />
+    <Images image={ImgRight} alt={alt} class="content-column-images-left" />
   );
 };
 
